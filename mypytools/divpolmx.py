0869f318993fbf66ce8ca5a60617eb4667db1a1e
Roberto A. Real Rangel <rrealr@iingen.unam.mx>
2020-04-08 14:18:15 -0700

    Cambios menores
    * En el módulo mypytools/divpolmx, los DataFrames de pandas fueron sustituidos por diccionarios de Python.

--- mypytools/divpolmx.py
+++ mypytools/divpolmx.py
@@ -6,35 +6,85 @@ Created on Fri Mar 27 23:37:52 2020
 """
 import pandas as _pd
 
-edosmx_iso = _pd.DataFrame(
-    data={'ISO 3166-2':
-          ['AGU', 'BCN', 'BCS', 'CAM', 'CHP', 'CHH', 'CMX', 'COA', 'COL',
-           'DUR', 'GUA', 'GRO', 'HID', 'JAL', 'MEX', 'MIC', 'MOR', 'NAY',
-           'NLE', 'OAX', 'PUE', 'QUE', 'ROO', 'SLP', 'SIN', 'SON', 'TAB',
-           'TAM', 'TLA', 'VER', 'YUC', 'ZAC']
-          },
-    index=['Aguascalientes', 'Baja California', 'Baja California Sur',
-          'Campeche', 'Chiapas', 'Chihuahua', 'Ciudad de México',
-          'Coahuila', 'Colima', 'Durango', 'Guanajuato', 'Guerrero', 'Hidalgo',
-          'Jalisco', 'México', 'Michoacán', 'Morelos', 'Nayarit', 'Nuevo León',
-          'Oaxaca', 'Puebla', 'Querétaro', 'Quintana Roo', 'San Luis Potosí',
-          'Sinaloa', 'Sonora', 'Tabasco', 'Tamaulipas', 'Tlaxcala', 'Veracruz',
-          'Yucatán', 'Zacatecas'],
-    )
+def iso31662mx():
+    """Códigos ISO 3166-2 para México.
 
-edosmx_abr = _pd.DataFrame(
-    data={'ABREVIATURA':
-          ['Ags.',  'B.C.', 'B.C.S.', 'Camp.', 'Chis.', 'Chih.', 'C.D.Mx.',
-           'Coah.', 'Col.', 'Dgo.', 'Gto.', 'Gro.', 'Hgo.', 'Jal.',
-           'Edo. Méx.', 'Mich.', 'Mor.', 'Nay.', 'N.L.', 'Oax.', 'Pue.',
-           'Qro.', 'Q. Roo.', 'S.L.P.', 'Sin.', 'Son.', 'Tab.', 'Tamps.',
-           'Tlax.', 'Ver.', 'Yuc.', 'Zac.']
-          },
-    index=['Aguascalientes', 'Baja California', 'Baja California Sur',
-          'Campeche', 'Chiapas', 'Chihuahua', 'Ciudad de México',
-          'Coahuila', 'Colima', 'Durango', 'Guanajuato', 'Guerrero', 'Hidalgo',
-          'Jalisco', 'México', 'Michoacán', 'Morelos', 'Nayarit', 'Nuevo León',
-          'Oaxaca', 'Puebla', 'Querétaro', 'Quintana Roo', 'San Luis Potosí',
-          'Sinaloa', 'Sonora', 'Tabasco', 'Tamaulipas', 'Tlaxcala', 'Veracruz',
-          'Yucatán', 'Zacatecas'],
-    )
+    Códigos para la representación de nombres de países y sus
+    subdivisiones - Parte 2: Código de subdivisión País.
+    """
+    return {
+        'Aguascalientes': 'AGU',
+        'Baja California': 'BCN',
+        'Baja California Sur': 'BCS',
+        'Campeche': 'CAM',
+        'Chiapas': 'CHP',
+        'Chihuahua': 'CHH',
+        'Ciudad de México': 'CMX',
+        'Coahuila': 'COA',
+        'Colima': 'COL',
+        'Durango': 'DUR',
+        'Guanajuato': 'GUA',
+        'Guerrero': 'GRO',
+        'Hidalgo': 'HID',
+        'Jalisco': 'JAL',
+        'México': 'MEX',
+        'Michoacán': 'MIC',
+        'Morelos': 'MOR',
+        'Nayarit': 'NAY',
+        'Nuevo León': 'NLE',
+        'Oaxaca': 'OAX',
+        'Puebla': 'PUE',
+        'Querétaro': 'QUE',
+        'Quintana Roo': 'ROO',
+        'San Luis Potosí': 'SLP',
+        'Sinaloa': 'SIN',
+        'Sonora': 'SON',
+        'Tabasco': 'TAB',
+        'Tamaulipas': 'TAM',
+        'Tlaxcala': 'TLA',
+        'Veracruz': 'VER',
+        'Yucatán': 'YUC',
+        'Zacatecas': 'ZAC'
+        }
+
+
+def edosmx_abr():
+    """Códigos ISO 3166-2 para México.
+
+    Códigos para la representación de nombres de países y sus
+    subdivisiones - Parte 2: Código de subdivisión País.
+    """
+    return {
+        'Aguascalientes': 'Ags.',
+        'Baja California': 'B.C.',
+        'Baja California Sur': 'B.C.S.',
+        'Campeche': 'Cam.',
+        'Chiapas': 'Chis.',
+        'Chihuahua': 'Chih.',
+        'Ciudad de México': 'Cd.Mx.',
+        'Coahuila': 'Coah.',
+        'Colima': 'Col.',
+        'Durango': 'Dgo.',
+        'Guanajuato': 'Gto.',
+        'Guerrero': 'Gro.',
+        'Hidalgo': 'Hgo.',
+        'Jalisco': 'Jal.',
+        'México': 'Méx.',
+        'Michoacán': 'Mich.',
+        'Morelos': 'Mor.',
+        'Nayarit': 'Nay.',
+        'Nuevo León': 'N.L.',
+        'Oaxaca': 'Oax.',
+        'Puebla': 'Pue.',
+        'Querétaro': 'Qro.',
+        'Quintana Roo': 'Q. Roo',
+        'San Luis Potosí': 'S.L.P.',
+        'Sinaloa': 'Sin.',
+        'Sonora': 'Son',
+        'Tabasco': 'Tab.',
+        'Tamaulipas': 'Tamps.',
+        'Tlaxcala': 'Tlax.',
+        'Veracruz': 'Ver.',
+        'Yucatán': 'Yuc.',
+        'Zacatecas': 'Zac.'
+        }
